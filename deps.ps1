1c7e3cc73ffaac7623756747baa2f44fe4b187a2
Desmond Naranjo User <dhnaranjo@gmail.com>
2017-08-06 15:59:16 -0700

    Updated deps

--- deps.ps1
+++ deps.ps1
@@ -43,17 +43,18 @@ choco install webpi               --limit-output
 choco install git                 --limit-output -params '"/GitAndUnixToolsOnPath /NoShellIntegration"'
 choco install nvm                 --limit-output
 choco install ruby                --limit-output
+choco install poshgit             --limit-output
 
 # browsers
 choco install GoogleChrome        --limit-output
 choco install Firefox             --limit-output
-choco install Opera               --limit-output
 
 # dev tools and frameworks
 choco install visualstudiocode    --limit-output
-choco install Fiddler4            --limit-output
 choco install vim                 --limit-output
-choco install winmerge            --limit-output
+choco install docker-for-windows  --limit-output
+choco install yarn                --limit-output
+choco install elixir              --limit-output
 
 Refresh-Environment
 
@@ -66,71 +67,13 @@ Remove-Variable nodeLtsVersion
 gem pristine --all --env-shebang
 
 ### Windows Features
-Write-Host "Installing Windows Features..." -ForegroundColor "Yellow"
-# # IIS Base Configuration
-# Enable-WindowsOptionalFeature -Online -All -FeatureName `
-#     "IIS-BasicAuthentication", `
-#     "IIS-DefaultDocument", `
-#     "IIS-DirectoryBrowsing", `
-#     "IIS-HttpCompressionDynamic", `
-#     "IIS-HttpCompressionStatic", `
-#     "IIS-HttpErrors", `
-#     "IIS-HttpLogging", `
-#     "IIS-ISAPIExtensions", `
-#     "IIS-ISAPIFilter", `
-#     "IIS-ManagementConsole", `
-#     "IIS-RequestFiltering", `
-#     "IIS-StaticContent", `
-#     "IIS-WebSockets", `
-#     "IIS-WindowsAuthentication" `
-#     -NoRestart | Out-Null
-
-# # ASP.NET Base Configuration
-# Enable-WindowsOptionalFeature -Online -All -FeatureName `
-#     "NetFx3", `
-#     "NetFx4-AdvSrvs", `
-#     "NetFx4Extended-ASPNET45", `
-#     "IIS-NetFxExtensibility", `
-#     "IIS-NetFxExtensibility45", `
-#     "IIS-ASPNET", `
-#     "IIS-ASPNET45" `
-#     -NoRestart | Out-Null
 
 # Web Platform Installer for remaining Windows features
-webpicmd /Install /AcceptEula /Products:"UrlRewrite2"
+#webpicmd /Install /AcceptEula /Products:"UrlRewrite2"
 #webpicmd /Install /AcceptEula /Products:"NETFramework452"
-webpicmd /Install /AcceptEula /Products:"Python279"
+#webpicmd /Install /AcceptEula /Products:"Python279"
 
 ### Node Packages
 Write-Host "Installing Node Packages..." -ForegroundColor "Yellow"
-if (which npm) {
-    npm update npm
-    npm install -g gulp
-    npm install -g mocha
-    npm install -g node-inspector
-    npm install -g yo
-}
-
-# ### Janus for vim
-# Write-Host "Installing Janus..." -ForegroundColor "Yellow"
-# if ((which curl) -and (which vim) -and (which rake) -and (which bash)) {
-#     curl.exe -L https://bit.ly/janus-bootstrap | bash
-# }
-
-
-### Visual Studio Plugins
-if (which Install-VSExtension) {
-    ### Visual Studio 2015
-    # VsVim
-    # Install-VSExtension https://visualstudiogallery.msdn.microsoft.com/59ca71b3-a4a3-46ca-8fe1-0e90e3f79329/file/6390/57/VsVim.vsix
-    # Productivity Power Tools 2015
-    # Install-VSExtension https://visualstudiogallery.msdn.microsoft.com/34ebc6a2-2777-421d-8914-e29c1dfa7f5d/file/169971/1/ProPowerTools.vsix
-
-    ### Visual Studio 2013
-    # VsVim
-    # Install-VSExtension https://visualstudiogallery.msdn.microsoft.com/59ca71b3-a4a3-46ca-8fe1-0e90e3f79329/file/6390/57/VsVim.vsix
-    # Productivity Power Tools 2013
-    # Install-VSExtension https://visualstudiogallery.msdn.microsoft.com/dbcb8670-889e-4a54-a226-a48a15e4cace/file/117115/4/ProPowerTools.vsix
-    # Web Essentials 2013
-    # Install-VSExtension https://visualstudiogallery.msdn.microsoft.com/56633663-6799-41d7-9df7-0f2a504ca361/file/105627/47/WebEssentials2013.vsix
+if (which yarn) {
 }
